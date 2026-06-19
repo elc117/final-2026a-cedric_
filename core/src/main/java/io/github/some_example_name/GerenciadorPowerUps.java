8706c6ab5967bd4c4ffcfa95c7e7fe60416cc542
cedricmr <cedricmarquesrocha@gmail.com>
2026-06-19 19:02:20 -0300

    feat: reduz frequencia de spawn dos powerups

--- core/src/main/java/io/github/some_example_name/GerenciadorPowerUps.java
+++ core/src/main/java/io/github/some_example_name/GerenciadorPowerUps.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.utils.Array;
 
 public class GerenciadorPowerUps {
     private float tempoDesdeSpawn = 0;
-    private float intervaloSpawn = 8f;
+    private float intervaloSpawn = 16f;
 
     public void atualizar(float delta, Array<PowerUp> powerups) {
         tempoDesdeSpawn = tempoDesdeSpawn + delta;
